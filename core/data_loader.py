ced50438523e74d898bc4bac67ca5706ee6e1417
Pääkkönen Juho <paakkoj6@t31300-la048.org.aalto.fi>
2016-12-22 16:00:50 +0200

    Add method for formatting data to common format

--- core/data_loader.py
+++ core/data_loader.py
@@ -23,14 +23,24 @@ def __harmonize_data( data ):
     ## make dates as date objects
     data2 = []
     for d in data:
-        if 'created_time' in d:
+
+        d = __format_data( d )
+
+        if '_created_time' in d:
            #d['date'] = dateparser.parse( d['created_time'] ) ## should take care of the various formats
 
-           d['date'] = datetime.strptime( d['created_time'].replace( 'T', ' ' ).replace( '+0000', '' ), '%Y-%m-%d %H:%M:%S' )
-           d['creator'] = d['from']['name']
+           d['timestamp'] = datetime.strptime( d['_created_time'].replace( 'T', ' ' ).replace( '+0000', '' ), '%Y-%m-%d %H:%M:%S' )
+           d['creator'] = d['_from']['name']
            data2.append( d )
 
     return data2
+
+def __format_data( data ):
+    formatted_data = {}
+    for key in data.keys():
+        formatted_data['_' + key] = data[key]
+
+    return formatted_data
 
 def load_facebook( terms = ['data_'], data_folder = 'facebook/' ): ## todo: better filtering
 
@@ -41,8 +51,6 @@ def load_facebook( terms = ['data_'], data_folder = 'facebook/' ): ## todo: bett
     for f in os.listdir( path ):
 
         if any( term in f for term in terms ):
-
-            #print json.load( open( __DATA_DIR + f ) ).keys()
 
             data += json.load( open( path + f ) )['feed']
 
@@ -63,10 +71,15 @@ def load_media( terms = ['.json'], data_folder = 'media/' ):
             d = pickle.load( open( path + f ) )
 
             ## TBA: change to standart format correctly
-            d['date'] = min( d['datetime_list'] )
+            d['timestamp'] = min( d['datetime_list'] )
             d['creator'] = d['author']
-            d['message'] = d['title'] + ' ' + d['ingress'] + ' ' + d['text']
+            d['text_content'] = d['title'] + ' ' + d['ingress'] + ' ' + d['text']
 
             data.append( d )
 
     return data
+
+
+if __name__ == '__main__':
+    data = load_facebook(['nokkahuilu'])
+    print data[0].keys()
