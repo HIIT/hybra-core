5739b23d9f2a66780f621b687c557955a7bbac5b
Pääkkönen Juho <paakkoj6@t31300-la048.org.aalto.fi>
2017-01-17 09:32:39 +0200

    Timeline to start from zero, closes #5

--- core/timeline.py
+++ core/timeline.py
@@ -1,17 +1,25 @@
 from collections import Counter
-import datetime
+from datetime import date, timedelta
 import codecs
 from string import Template
 
 def create_timeline(data):
 
-    dates = map( lambda d: str( d['timestamp'].date() ), filter( lambda d: d['timestamp'] is not '', data ) )
+    dates = map( lambda d: d['timestamp'].date(), filter( lambda d: d['timestamp'] is not '', data ) )
     timeline_data = Counter( dates )
 
-    x_axis = sorted( timeline_data )
+    start = min( timeline_data )
+    end = max( timeline_data )
+    delta = end - start
+
+    x_axis = map( lambda date: start + timedelta( days = date ), range( delta.days + 1 ) )
+
     formatted_data = []
     for date in x_axis:
-        formatted_data.append({'close' : timeline_data[date], 'date' : date})
+        if date in timeline_data:
+            formatted_data.append({'close' : timeline_data[date], 'date' : str(date)})
+        else:
+            formatted_data.append({'close' : 0, 'date' : str(date)})
 
     css_text = codecs.open('css/timeline.css', 'r').read()
 
@@ -26,12 +34,17 @@ def create_timeline(data):
 
 if __name__ == '__main__':
 
-    for function_name in dir( data_loader ):
+    import data_loader
+    data = data_loader.load_facebook( ['nokkahuilu'] )
 
-        if 'load_' in function_name:
+    create_timeline(data)
 
-            print function_name
-            f =  getattr( data_loader, function_name )
-            data = f()
-            create_timeline( data )
-            plt.show()
+    #for function_name in dir( data_loader ):
+
+        #if 'load_' in function_name:
+
+            #print function_name
+            #f =  getattr( data_loader, function_name )
+            #data = f()
+            #create_timeline( data )
+            #plt.show()
