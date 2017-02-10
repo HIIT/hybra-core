fe2edae3094782b2ccd5016e9d8b5d5b6e7ef28b
Pääkkönen Juho <paakkoj6@t31300-la048.org.aalto.fi>
2017-02-10 12:11:09 +0200

    Support for multiple plots and color parameters

--- core/timeline.py
+++ core/timeline.py
@@ -6,11 +6,39 @@ path = os.path.dirname(os.path.abspath(__file__))
 from collections import Counter
 from datetime import date, timedelta
 import codecs
+import time
 
 from string import Template
 
-def create_timeline(data):
+def create_timeline( datasets = [], colors = [] ):
 
+    if not datasets: return "No data given to create timeline."
+
+    plots = create_plots( datasets )
+
+    graph_div_id = int( time.time() * 1000 )
+
+    html_template = create_html_template( graph_div_id )
+    js_template = Template( codecs.open( path + '/js/timeline.js', 'r').read() )
+
+    css_text = create_css_text()
+    js_text = js_template.substitute( {'graph_div_id' : graph_div_id,
+                                      'plots' : plots,
+                                      'line_colors' : colors} )
+
+    return html_template.substitute( {'css': css_text, 'js': js_text} )
+
+
+def create_plots( datasets ):
+    plots = []
+    for data in datasets:
+        x_axis, y_axis = create_axes( data )
+        plots.append( create_data_points( x_axis, y_axis ) )
+
+    return plots
+
+
+def create_axes( data ):
     dates = map( lambda d: d['timestamp'].date(), filter( lambda d: d['timestamp'] is not '', data ) )
 
     y_axis = Counter( dates )
@@ -21,15 +49,7 @@ def create_timeline(data):
 
     x_axis = map( lambda date: start_date + timedelta( days = date ), range( delta.days + 1 ) )
 
-    data_points = create_data_points( x_axis, y_axis )
-
-    js_text_template = Template( codecs.open( path + '/js/timeline.js', 'r').read() )
-    html_template = Template( codecs.open( path + '/html/timeline.html', 'r').read() )
-
-    css_text = codecs.open( path + '/css/timeline.css', 'r').read()
-    js_text = js_text_template.substitute({'data' : data_points})
-
-    return html_template.substitute( {'css': css_text, 'js': js_text} )
+    return x_axis, y_axis
 
 
 def create_data_points( x_axis, y_axis ):
@@ -42,3 +62,32 @@ def create_data_points( x_axis, y_axis ):
             data_points.append({'close' : 0, 'date' : str(date)})
 
     return data_points
+
+
+def create_html_template( graph_div_id ):
+
+    html_template = Template('''
+        <style> $css </style>
+        <div id="timeline_graph_''' + str(graph_div_id) + '''"></div>
+        <script> $js </script>
+    ''')
+
+    return html_template
+
+
+def create_css_text():
+    css_text = '''
+        path {
+            stroke-width: 2;
+            fill: none;
+        }
+
+        .axis path, .axis line {
+            fill: none;
+            stroke: grey;
+            stroke-width: 1;
+            shape-rendering: crispEdges;
+        }
+    '''
+
+    return css_text
