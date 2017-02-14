c920b5e121e07cb8570b1fe503994ceb1d75c90b
Pääkkönen Juho <paakkoj6@t31300-la048.org.aalto.fi>
2017-02-14 16:37:31 +0200

    Refactor timeline.js

--- core/timeline/timeline.js
+++ core/timeline/timeline.js
@@ -1,11 +1,17 @@
+var plots = $plots;
+var line_colors = $line_colors;
+
+
 // Set the dimensions of the canvas / graph
 var margin = {top: 30, right: 20, bottom: 30, left: 50},
     width = 900 - margin.left - margin.right,
     height = 400 - margin.top - margin.bottom;
 
+
 // Set the ranges
 var x = d3.time.scale().range([0, width]);
 var y = d3.scale.linear().range([height, 0]);
+
 
 // Define the axes
 var xAxis = d3.svg.axis().scale(x)
@@ -14,10 +20,12 @@ var xAxis = d3.svg.axis().scale(x)
 var yAxis = d3.svg.axis().scale(y)
     .orient("left").ticks(5);
 
+
 // Define the line
 var valueline = d3.svg.line()
     .x(function(d) { return x(d.date); })
     .y(function(d) { return y(d.close); });
+
 
 // Adds the svg canvas
 var svg = d3.select("#timeline_graph_" + $graph_div_id)
@@ -28,15 +36,14 @@ var svg = d3.select("#timeline_graph_" + $graph_div_id)
         .attr("transform",
               "translate(" + margin.left + "," + margin.top + ")");
 
-var plots = $plots;
-var line_colors = $line_colors;
 
 var y_max = 0;
 var x_range = new Array(2);
 
-for (i = 0; i < plots.length; i++) {
-    var data = plots[i];
+// Find out plot ranges
+plots.forEach(function(data) {
 
+    // Format plot dates
     data.forEach(function(d) {
         d.date = new Date(d.date);
         d.close = +d.close;
@@ -61,8 +68,11 @@ for (i = 0; i < plots.length; i++) {
     if (y_max < y_largest) {
       y_max = y_largest;
     }
-}
 
+});
+
+
+// Scale the plots and determine line colors
 for (i = 0; i < plots.length; i++) {
     var data = plots[i];
 
@@ -84,6 +94,7 @@ for (i = 0; i < plots.length; i++) {
         .attr("stroke", color)
         .attr("d", valueline(data));
 }
+
 
 // Add the X Axis
 svg.append("g")
