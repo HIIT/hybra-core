f12f4087b857db6282252da390ca7d1e4115ec5b
Pääkkönen Juho <juho.paakkonen@helsinki.fi>
2017-03-20 11:38:46 +0200

    Use alabaster theme

--- docs/conf.py
+++ docs/conf.py
@@ -20,8 +20,6 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../core'))
 
-import sphinx_rtd_theme
-
 
 # -- General configuration ------------------------------------------------
 
@@ -89,9 +87,7 @@ todo_include_todos = True
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-#html_theme = 'alabaster'
-html_theme = "sphinx_rtd_theme"
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_theme = 'alabaster'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
