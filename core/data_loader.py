1694fc27a9c77cc70d6664897ef4513688f39c2f
Pääkkönen Juho <juho.paakkonen@helsinki.fi>
2017-04-03 16:41:23 +0300

    Refactor data loaders

--- core/data_loader.py
+++ core/data_loader.py
@@ -30,7 +30,7 @@ def _version( folder ):
         print( "\t Data is not stored in a repo. Data might not be up-to-date!" )
 
 
-def __harmonize_data( data, data_type ):
+def __harmonize_data( data, data_type, common_data_keys ):
 
     harmonized_data = {}
 
@@ -39,13 +39,30 @@ def __harmonize_data( data, data_type ):
 
     harmonized_data['source'] = data_type
     harmonized_data['creator'] = ''
-    harmonized_data['timestamp'] = datetime.strptime( '1970-01-01 00:00:00', '%Y-%m-%d %H:%M:%S' )
+    harmonized_data['timestamp'] = ''
     harmonized_data['text_content'] = ''
     harmonized_data['url'] = ''
     harmonized_data['source_detail'] = ''
     harmonized_data['images'] = []
     harmonized_data['links'] = []
     harmonized_data['broken'] = {}
+
+    for key, value in common_data_keys.items():
+        try:
+            if type(key) is tuple:
+                harmonized_data[value] += harmonized_data[key[0]][key[1]]
+            else:
+                harmonized_data[value] += harmonized_data[key] + ' '
+
+            harmonized_data[value] = harmonized_data[value].strip()
+
+        except Exception, e:
+            harmonized_data['broken'][value] = e
+
+    if not harmonized_data['timestamp']:
+        harmonized_data['timestamp'] = '1970-01-01 00:00:00'
+
+    harmonized_data['timestamp'] = dateparser.parse( harmonized_data['timestamp'] )
 
     return harmonized_data
 
@@ -66,22 +83,11 @@ def load_facebook( terms = ['.json'], data_folder = 'facebook/' ): ## todo: bett
 
             for d in dump['feed']:
 
-                d = __harmonize_data( d, 'facebook' )
+                common_data_keys = {('_from', 'name') : 'creator',
+                                    '_created_time' : 'timestamp',
+                                    '_message' : 'text_content'}
 
-                try:
-                    d['creator'] = d['_from']['name']
-                except Exception, e:
-                    d['broken']['creator'] = e
-
-                try:
-                    d['timestamp'] = dateparser.parse( d['_created_time'] ) ## should take care of the various formats
-                except Exception, e:
-                    d['broken']['timestamp'] = e
-
-                try:
-                    d['text_content'] = d['_message']
-                except Exception, e:
-                    d['broken']['text_content'] = e
+                d = __harmonize_data( d, 'facebook', common_data_keys )
 
                 d['url'] = 'https://www.facebook.com/' + d['_id']
 
@@ -114,26 +120,24 @@ def load_media( terms = ['.json'], data_folder = 'media/' ):
 
             for d in json.load( open( path + f ) ):
 
-                d = __harmonize_data( d, 'news_media' )
+                common_data_keys = {'_author' : 'creator',
+                                    '_title' : 'text_content',
+                                    '_ingress' : 'text_content',
+                                    '_text' : 'text_content',
+                                    '_url' : 'url',
+                                    '_domain' : 'source_detail',
+                                    '_images' : 'images'}
 
-                d['creator'] = d['_author']
+                d = __harmonize_data( d, 'news_media', common_data_keys )
 
                 ## ensure data is always in a list
                 if isinstance( d['_datetime_list'] , str) or isinstance( d['_datetime_list']  , unicode):
                     d['_datetime_list'] = [ d['_datetime_list'] ]
 
-                d['timestamp'] = dateparser.parse( min( d['_datetime_list'] ), ) ## should take care of the various formats
-
-                d['text_content'] = d['_title'] + ' ' + d['_ingress'] + ' ' + d['_text']
-
-                d['url'] = d['_url']
-
                 try:
-                    d['source_detail'] = d['_domain']
+                    d['timestamp'] = dateparser.parse( min( d['_datetime_list'] ), ) ## should take care of the various formats
                 except Exception, e:
-                    d['broken']['source_detail'] = e
-
-                d['images'] = d['_images']
+                    d['broken']['_datetime_list'] = e
 
                 data.append(d)
 
@@ -163,29 +167,25 @@ def load_twitter( terms = ['data_'], data_folder = 'twitter/' ):
 
             for d in unharmonized_data:
 
-                d = __harmonize_data( d, 'twitter' )
+                unharmonized_data = []
 
-                try:
-                    d['creator'] = d['_user']['screen_name']
-                except Exception, e:
-                    d['broken']['creator'] = e
+                with open( path + f ) as current_file:
+                    unharmonized_data = json.load( current_file )
 
-                try:
-                    d['timestamp'] = dateparser.parse( d['_created_at'] ) ## should take care of the various formats
-                except Exception, e:
-                    d['broken']['timestamp'] = e
+                for d in unharmonized_data:
 
-                try:
-                    d['text_content'] = d['_text']
-                except Exception, e:
-                    d['broken']['text_content'] = e
+                    common_data_keys = {('_user', 'screen_name') : 'creator',
+                                        '_created_at' : 'timestamp',
+                                        '_text' : 'text_content'}
 
-                try:
-                    d['url'] = 'https://www.twitter.com/statuses/' + d['_id_str']
-                except Exception, e:
-                    d['broken']['url'] = e
+                    d = __harmonize_data( d, 'twitter', common_data_keys )
 
-                data.append(d)
+                    try:
+                        d['url'] = 'https://www.twitter.com/statuses/' + d['_id_str']
+                    except Exception, e:
+                        d['broken']['url'] = e
+
+                    data.append(d)
 
     return data
 
@@ -199,113 +199,40 @@ def load_futusome( query, data_folder = 'futusome/', api_key = '', check_documen
     if not os.path.exists( path ):
         os.makedirs( path )
 
-    query_string = 'https://api.futusome.com/api/searches.json?'
+    query_base = 'https://api.futusome.com/api/searches.json?&api_search[query]='
 
-    query_string += '&api_search[query]=' + query
-
-    unharmonized_data = load_unharmonized_futusome_data( query_string, api_key, path, check_document_count, override_cache )
-
-    if not unharmonized_data: return data
-
-    for d in unharmonized_data['documents']:
-
-        d = __harmonize_data( d['fields'], 'futusome' )
-
-        try:
-            d['creator'] = d['_author']
-        except Exception, e:
-            d['broken']['creator'] = e
-
-        try:
-            d['timestamp'] = dateparser.parse(d['_published']).replace(tzinfo=None)
-        except Exception, e:
-            d['broken']['timestamp'] = e
-
-        d['text_content'] = ''
-        text_content_errors = []
-        try:
-            d['text_content'] += d['_name'] + ' '
-        except Exception, e:
-            text_content_errors.append(e)
-            d['broken']['text_content'] = text_content_errors
-
-        try:
-            d['text_content'] += d['_text']
-        except Exception, e:
-            text_content_errors.append[e]
-            d['broken']['text_content'] = text_content_errors
-
-        url_errors = []
-        try:
-            d['url'] = d['_blog_id']
-        except Exception, e:
-            url_errors.append(e)
-            d['broken']['url'] = url_errors
-
-        if d['url'] == '':
-            try:
-                d['url'] = d['_url']
-            except Exception, e:
-                url_errors.append(e)
-                d['broken']['url'] = url_errors
-
-        try:
-            d['source_detail'] = d['_type']
-        except Exception, e:
-            d['broken']['source_detail'] = e
-
-        if '_forum_post_id' in d:
-            d['_id'] = d['_forum_post_id']
-        elif '_twitter_retweet_id' in d:
-            d['_id'] = 'twitter_' + d['_twitter_retweet_id']
-        elif '_facebook_id' in d:
-            d['_id'] = 'facebook_' + d['_facebook_id']
-        elif '_twitter_tweet_id' in d:
-            d['_id'] = 'twitter_' + d['_twitter_tweet_id']
-        elif '_url' in d:
-            ## make uniq ID ourself
-            text =  d['_url'].encode('ascii', 'ignore') + str( d['timestamp'] ) + d['text_content'].encode('ascii', 'ignore')
-            d['_id'] = 'created_id_' + hashlib.md5( text ).hexdigest()
-        else:
-           text =  str( d['timestamp'] ) + d['text_content'].encode('ascii', 'ignore')
-           d['_id'] = 'created_id_' + hashlib.md5( text ).hexdigest()
-
-        data.append(d)
-
-    return data
+    cache_file = query.replace('/', '_') # Slashes not allowed in filenames
 
 
-def load_unharmonized_futusome_data( query_string, api_key, data_path, check_document_count, override_cache ):
-
+    # If just checking document count, query for only one document
     if check_document_count:
-        r = requests.get( query_string + '&api_key=' + api_key + '&api_search[limit]=1' )
+        r = requests.get( query_base + query + '&api_key=' + api_key + '&api_search[limit]=1' )
         r =  r.json()
         print('Total document count: ' + str(r['count']))
         return
 
-    query_base = 'https://api.futusome.com/api/searches.json?'
 
     unharmonized_data = {}
 
-    cache_file = query_string.replace(query_base, '')
-    cache_file = cache_file.replace('&api_search[query]=', '')
-    cache_file = cache_file.replace('/', '_')
 
+    # Check if data matching the query is cached in the data path
     if not override_cache:
 
         print( "Checking local data path for cached data..." )
 
-        for f in os.listdir( data_path ):
+        for f in os.listdir( path ):
 
             if cache_file == f.replace('.json', ''):
-                print("Data returned from " + data_path)
+                print("Data returned from " + path)
 
-                with open( data_path + '/' + f ) as current_file:
+                with open( path + '/' + f ) as current_file:
                     unharmonized_data = json.load( current_file )
 
+
+    # If data not found in cache, query Futusome API
     if api_key and not unharmonized_data:
 
-        print( "Data not returned from cache. Querying Futusome api..." )
+        print( "Data not returned from cache. Querying Futusome API..." )
 
         documents = []
 
@@ -327,7 +254,7 @@ def load_unharmonized_futusome_data( query_string, api_key, data_path, check_doc
                 min_date = str( int( min_date.strftime('%s') ) * 1000 )
                 time = ' AND indexed.at:['  + min_date + ' TO ' + max_date + ']' ## could also be indexed at? is it faster?
 
-            r = requests.get( query_string + time + '&api_key=' + api_key + '&api_search[limit]=5000&api_search[sort]=indexed.at' )
+            r = requests.get( query_base + query + time + '&api_key=' + api_key + '&api_search[limit]=5000&api_search[sort]=indexed.at' )
             r =  r.json()
 
             if 'error' in r:
@@ -348,7 +275,46 @@ def load_unharmonized_futusome_data( query_string, api_key, data_path, check_doc
 
         unharmonized_data = {'documents' : documents}
 
-        json.dump( unharmonized_data , open(  data_path + '/' + cache_file + '.json', 'w' ) )
-        print('Data saved to ' + data_path + '/' + cache_file + '.json')
+        # Save data in local cache with the query as filename
+        json.dump( unharmonized_data , open(  path + '/' + cache_file + '.json', 'w' ) )
+        print('Data saved to ' + path + '/' + cache_file + '.json')
 
-    return unharmonized_data
+
+    # If no data found in cache or Futusome, just return
+    if not unharmonized_data: return data
+
+
+    # Harmonize data to common format and return it
+    for d in unharmonized_data['documents']:
+
+        common_data_keys = {'_author' : 'creator',
+                            '_published' : 'timestamp',
+                            '_name' : 'text_content',
+                            '_text' : 'text_content',
+                            '_url' : 'url',
+                            '_blog_id' : 'url',
+                            '_type' : 'source_detail'}
+
+        d = __harmonize_data( d['fields'], 'futusome', common_data_keys )
+
+        d['timestamp'] = d['timestamp'].replace(tzinfo = None)
+
+        if '_forum_post_id' in d:
+            d['_id'] = d['_forum_post_id']
+        elif '_twitter_retweet_id' in d:
+            d['_id'] = 'twitter_' + d['_twitter_retweet_id']
+        elif '_facebook_id' in d:
+            d['_id'] = 'facebook_' + d['_facebook_id']
+        elif '_twitter_tweet_id' in d:
+            d['_id'] = 'twitter_' + d['_twitter_tweet_id']
+        elif '_url' in d:
+            ## make uniq ID ourself
+            text =  d['_url'].encode('ascii', 'ignore') + str( d['timestamp'] ) + d['text_content'].encode('ascii', 'ignore')
+            d['_id'] = 'created_id_' + hashlib.md5( text ).hexdigest()
+        else:
+           text =  str( d['timestamp'] ) + d['text_content'].encode('ascii', 'ignore')
+           d['_id'] = 'created_id_' + hashlib.md5( text ).hexdigest()
+
+        data.append(d)
+
+    return data
