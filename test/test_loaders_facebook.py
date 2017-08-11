636d1c683446b9dd40041f8e6003d76f49d269b6
Matti Nelimarkka <matti.nelimarkka@hiit.fi>
2017-08-11 14:02:56 +0300

    Add simple Facebook test loader

---
+++ test/test_loaders_facebook.py
@@ -0,0 +1,39 @@
+import os, sys
+
+sys.path.append('../core/')
+
+import hybra
+
+class TestUM:
+
+    def setup(self):
+        self.data = hybra.data( 'facebook', data_folder = '', terms = ['facebook.json'] )
+        self.listdata = list( self.data )
+
+    def test_get_everything(self):
+
+        assert len( self.data ) == 4
+
+    def test_links(self):
+        assert len( self.listdata[0]['links'] ) == 4
+
+        for i, j in enumerate( "http://www.google.com,http://www.hs.fi,http://www.hiit.fi,http://www.google.com/".split(',') ):
+            assert self.listdata[0]['links'][ i ] == j
+
+        ## post with no links should be explicit
+        assert len( self.listdata[1]['links'] ) == 0
+
+    def test_texts(self):
+
+        texts = ["This is a post with a link http://www.google.com http://www.hs.fi http://www.hiit.fi",
+        "Post without comments",
+        "Post"]
+
+        for i, j in enumerate( texts ):
+            assert self.listdata[i]['text_content'] == j
+
+    def test_dates(self):
+
+        for entry in self.data:
+
+            assert entry['timestamp'] != None
