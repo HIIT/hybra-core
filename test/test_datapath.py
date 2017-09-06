d0046ae1dfc6c3cd86477180c3175562834c8f41
Pääkkönen Juho <juho.paakkonen@helsinki.fi>
2017-09-06 22:05:58 +0300

    Remove data load from test

--- test/test_datapath.py
+++ test/test_datapath.py
@@ -7,8 +7,7 @@ class TestUM:
 
     def setup(self):
 
-        hybra.set_data_path('./data2/')
-        self.g = hybra.data( 'news', folder = '', terms = ['yle.json'] )
+        hybra.set_data_path('./data_empty/')
 
     def test_is_changed( self ):
-        assert( hybra.data_path() == './data2/' )
+        assert( hybra.data_path() == './data_empty/' )
