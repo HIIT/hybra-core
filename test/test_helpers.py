64d86665098fce2154388b5318cb6003885dd49a
Pääkkönen Juho <paakkoj6@t31300-la048.org.aalto.fi>
2017-09-19 11:53:09 +0300

    Tests for filter method

--- test/test_helpers.py
+++ test/test_helpers.py
@@ -15,5 +15,61 @@ class TestUM:
         self.dataMedia = hybra.data( 'news', folder = '', terms = ['yle.json'] )
         self.dataFacebook = hybra.data( 'facebook', folder = '', terms = ['facebook.json'] )
 
+        self.dataFacebook = map( lambda x: x, self.dataFacebook )
+        self.dataMedia = map( lambda x: x, self.dataMedia )
+
     def test_filter_text( self ):
+
+        fb = hybra.filter_by( self.dataFacebook, 'text', text = [''] )
+        media = hybra.filter_by( self.dataMedia, 'text', text = [''] )
+
+        assert( len(fb) == len(self.dataFacebook) )
+        assert( len(media) == len(self.dataMedia) )
+
+        fb = hybra.filter_by( self.dataFacebook, 'text', text = ['Post'] )
+        media = hybra.filter_by( self.dataMedia, 'text', text = ['Algoritmi'] )
+
+        assert( len(fb) == 3 )
+        assert( len(media) == 1 )
+
+        fb = hybra.filter_by( self.dataFacebook, 'text', text = ['pos'] )
+        media = hybra.filter_by( self.dataMedia, 'text', text = ['algorit'] )
+
+        assert( len(fb) == 3)
+        assert( len(media) == 1)
+
+        fb = hybra.filter_by( self.dataFacebook, 'text', text = ['pos'], substrings = False )
+        media = hybra.filter_by( self.dataMedia, 'text', text = ['algorit'], substrings = False )
+
+        assert( len(fb) == 0 )
+        assert( len(media) == 0)
+
+        fb = hybra.filter_by( self.dataFacebook, 'text', text = ['post', 'missing text'] )
+        media = hybra.filter_by( self.dataMedia, 'text', text = ['algoritmi', 'missing text'] )
+
+        assert( len(fb) == 0 )
+        assert( len(media) == 0)
+
+        fb = hybra.filter_by( self.dataFacebook, 'text', text = ['post', 'missing text'], inclusive = False )
+        media = hybra.filter_by( self.dataMedia, 'text', text = ['algoritmi', 'missing text'], inclusive = False )
+
+        assert( len(fb) == 3 )
+        assert( len(media) == 1)
+
+    def test_filter_datetime( self ):
+        pass
+
+    def test_filter_authors( self ):
+        pass
+
+    def test_filter_domain( self ):
+        pass
+
+    def test_extract_domains( self ):
+        pass
+
+    def test_count_authors( self ):
+        pass
+
+    def test_count_domains( self ):
         pass
