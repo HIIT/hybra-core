8a5fd13121d939a1e3ef10536f3af19574faa2db
Matti Nelimarkka <matti.nelimarkka@hiit.fi>
2017-09-26 23:02:54 +0300

    First version of S24 data loader. Consider this alpha, not production quality, code.

---
+++ core/loaders/s24.py
@@ -0,0 +1,64 @@
+import os
+import json
+import datetime
+
+from lxml import etree
+
+path = './'
+
+
+
+
+for f in filter( lambda x: x.endswith('VRT2') or x.endswith('VRT'), os.listdir( path ) ):
+
+    data = open( path + f ).read()
+    out = []
+
+    ## fix to stringio and save some writing to hd
+
+    open('temp.xml', 'w').write("""<?xml version="1.0" encoding="UTF-8" ?>
+    <root>"""
+    + data +
+    "</root>")
+
+    for d in etree.iterparse( open('temp.xml') , tag = 'text', recover = True ):
+        d = d[1]
+
+        text = ''
+        text_lemma = ''
+
+        for p in d.findall('paragraph'):
+
+            for s in p.findall('sentence'):
+
+                for line in s.text.split('\n'):
+
+                    line = line.split('\t')
+
+                    if len( line ) > 3:
+
+                        text += line[0] + ' '
+                        text_lemma += line[2] + ' '
+
+
+
+        if 'rasis' in text or 'rasis' in text_lemma:
+
+            o = {}
+            o['text_content'] = d.get('title') + ' ' + text
+            o['text_content_lemma'] = text_lemma
+            o['source'] = 'Suomi24'
+            o['source_detail'] =  d.get('discussionarea') + '/' + d.get('subsections')
+            o['url'] = d.get('urlmsg')
+
+            dt = d.get('date').split('.') + d.get('time').split(':')
+            dt = map( int, dt )
+
+            o['timestamp'] = str( datetime.datetime( dt[2], dt[1], dt[0], dt[3], dt[4]) )
+
+            o['creator'] = d.get('anonnick')
+            o['id'] = d.get('tid') + '-' + d.get('cid')
+
+            out.append( o )
+
+    json.dump( out, open( f + '.json', 'w' ) )
