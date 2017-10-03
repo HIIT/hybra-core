064966d938fcd2cf3179214dfb695ad0b49f63e4
EeliH <eeli.harlas@gmail.com>
2017-10-03 14:14:28 +0300

    Added a new example

---
+++ docs/examples/yle_mostcommon.py
@@ -0,0 +1,13 @@
+from core import hybra
+from core.helpers import counters
+
+
+hybra.set_data_path('./data/')
+
+data = hybra.data( 'news', folder = '', terms = ['yle.json'] )
+
+authors = hybra.counts( data, 'author' )
+topauthors = authors.most_common(10)
+
+for name, number in topauthors:
+    print "%s (%s)" % (name, number)
