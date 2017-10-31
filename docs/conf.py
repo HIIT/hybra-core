dac6bd61251362b7fc24c24d25708c54bffba1f3
Pääkkönen Juho <juho.paakkonen@helsinki.fi>
2017-10-31 15:52:56 +0200

    Minor fix

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../hybra))
+sys.path.insert(0, os.path.abspath('../hybra'))
 
 
 # -- General configuration ------------------------------------------------
