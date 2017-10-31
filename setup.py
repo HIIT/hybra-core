25e97e204d6bdc07fe1bfcdd49bcef0c27491dc2
Pääkkönen Juho <juho.paakkonen@helsinki.fi>
2017-10-31 16:02:25 +0200

    Prepare package for PyPi

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+  name = 'hybra',
+  packages = ['hybra'],
+  version = '0.1',
+  description = 'A random test lib',
+  author = 'Matti Nelimarkka, Juho Pääkkönen, Arto Kekkonen',
+  author_email = 'matti.nelimarkka@aalto.fi, juho.paakkonen@aalto.fi, arto.kekkonen@helsinki.fi',
+  url = 'https://github.com/HIIT/hybra-core',
+  download_url = 'https://github.com/HIIT/hybra-core/archive/0.1.tar.gz',
+  keywords = ['data management', 'data analysis'],
+  classifiers = [],
+)
