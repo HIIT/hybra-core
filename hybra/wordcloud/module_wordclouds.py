08e269ec03ee5706094bff996d0c77f13f18f603
Pihla Toivanen <toivanenpihla@gmail.com>
2018-02-27 16:45:24 +0200

    Colors

--- hybra/wordcloud/module_wordclouds.py
+++ hybra/wordcloud/module_wordclouds.py
@@ -1,6 +1,7 @@
 import os
 import time
 from string import Template
+import random
 
 from IPython.core.display import HTML, display
 
@@ -8,7 +9,7 @@ from collections import Counter
 
 path = os.path.dirname(os.path.abspath(__file__))
 
-def create_wordcloud( data, plt, stopwords = ["the", "a", "or", "tai", "and", "ja", "to", "on", "in", "of", "for", "is", "i", "this", "http", "www", "fi", "com"] ):
+def create_wordcloud( data, plt, stopwords = ["the", "a", "or", "tai", "and", "ja", "to", "on", "in", "of", "for", "is", "i", "this", "http", "www", "fi", "com"], width=850, height=350 ):
 
     import codecs
     html_template = Template( codecs.open( path + '/wordcloud.html', 'r').read() )
@@ -28,11 +29,14 @@ def create_wordcloud( data, plt, stopwords = ["the", "a", "or", "tai", "and", "j
 
     frequencies = Counter(texts.split())
     freqs_list = []
+    colors = ["#A5E6BA", "#9AC6C5", "#7785AC", "#248757", "#360568", "#F0544F", "#e07f9f", "#1d7059", "#3e6282"]
+
     for key, value in frequencies.iteritems():
-        freqs_list.append({"text":encode_utf8(key),"size":str(value)})
+        freqs_list.append({"text":encode_utf8(key),"size":str(value), "color": random.choice(colors)})
 
     graph_div_id = int(time.time() * 1000)
-    js_text = js_template.substitute({'frequencies': str(freqs_list), 'graph_div_id': graph_div_id})
+
+    js_text = js_template.substitute({'frequencies': str(freqs_list), 'graph_div_id': graph_div_id, 'width': width, 'height': height})
     html_template = html_template.substitute({'js': js_text, 'graph_div_id': graph_div_id, 'css': css_text})
 
     display( HTML( html_template ) )
--- hybra/wordcloud/wordcloud.js
+++ hybra/wordcloud/wordcloud.js
@@ -398,11 +398,9 @@
     c.textAlign = "center";
 
     // wordcloud code ends
-
+    // ["#ddd", "#ccc", "#bbb", "#aaa", "#999", "#888", "#777", "#666", "#555", "#444", "#333", "#222"]
     var frequency_list = $frequencies
-    var color = d3.scale.linear()
-            .domain([0,1,2,3,4,5,6,10,15,20,100])
-            .range(["#ddd", "#ccc", "#bbb", "#aaa", "#999", "#888", "#777", "#666", "#555", "#444", "#333", "#222"]);
+
     cloud().size([800, 300])
             .words(frequency_list)
             .rotate(0)
@@ -411,8 +409,8 @@
             .start();
     function draw(words) {
         d3.select("#word_cloud_" + $graph_div_id).append("svg")
-                .attr("width", 850)
-                .attr("height", 350)
+                .attr("width", $width)
+                .attr("height", $height)
                 .attr("class", "wordcloud")
                 .append("g")
                 // without the transform, words words would get cutoff to the left and top, they would
@@ -422,7 +420,7 @@
                 .data(words)
                 .enter().append("text")
                 .style("font-size", function(d) { return d.size + "px"; })
-                .style("fill", function(d, i) { return color(i); })
+                .style("fill", function(d, i) { return d.color; })
                 .attr("transform", function(d) {
                     return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
                 })
