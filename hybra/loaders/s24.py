1a6d09f2341a8e84d675cf0fc0479188cd75f7bc
Matti Nelimarkka <matti.nelimarkka@hiit.fi>
2019-07-05 10:24:01 +0300

    Basic maintainance, updating per new folder structure

--- hybra/loaders/s24.py
+++ hybra/loaders/s24.py
@@ -5,16 +5,20 @@ import cStringIO
 
 from lxml import etree
 
-path = '/appl/kielipankki/Suomi24/'
+path = '/appl/kielipankki/Suomi24/2017H2/'
 
 files = filter( lambda x: x.endswith('VRT2') or x.endswith('VRT'), os.listdir( path ) )
+files = filter( lambda x: x.startswith('comments'), files )
 
 has_already = filter( lambda x: x.endswith('VRT2.json') or x.endswith('VRT.json'), os.listdir( '.' ) )
 has_already = map( lambda x: x.replace('.json', ''), has_already)
 
 files = set(files) - set(has_already)
 
-keywords = raw_input('Define keywords, separated by commas ')
+import sys
+keywords = sys.argv[1]
+
+print keywords
 
 outdir = keywords.replace(' ', '')
 
@@ -22,6 +26,8 @@ if not os.path.exists( outdir ):
     os.makedirs( outdir )
 
 keywords = keywords.split(',')
+
+total_sum = 0
 
 for f in files:
 
@@ -39,7 +45,7 @@ for f in files:
     + data +
     "</root>")
 
-    for d in etree.iterparse( open('temp.xml') , tag = 'text', recover = True ):
+    for d in etree.iterparse( open('temp.xml') , tag = 'text' , recover = True ):
         d = d[1]
 
         text = ''
@@ -59,7 +65,7 @@ for f in files:
                         text_lemma += line[2] + ' '
 
 
-        flag = False
+        flag = True
 
         for kw in keywords:
             if kw in text or kw in text_lemma:
@@ -77,16 +83,21 @@ for f in files:
             dt = d.get('date').split('.') + d.get('time').split(':')
             dt = map( int, dt )
 
-            o['timestamp'] = str( datetime.datetime( dt[2], dt[1], dt[0], dt[3], dt[4]) )
+            o['timestamp'] = datetime.datetime( dt[2], dt[1], dt[0], dt[3], dt[4])
 
             o['creator'] = d.get('anonnick')
             o['id'] = d.get('tid') + '-' + d.get('cid')
 
-            out.append( o )
+            if datetime.datetime( 2011, 1, 1, 0, 0, 1 ) <= o['timestamp'] <= datetime.datetime( 2015, 11, 15, 23, 59 ):
+               total_sum += 1
 
-    json.dump( out, open( outdir + '/' + f + '.json', 'w' ) )
-    print 'Done', f
+            ## out.append( o )
+
+    # json.dump( out, open( outdir + '/' + f + '.json', 'w' ) )
+    # print 'Done', f
 
  except Exception, e:
        print 'Failed', f
        print e
+
+open('total_sum.txt', 'w').write( str( total_sum ) )
